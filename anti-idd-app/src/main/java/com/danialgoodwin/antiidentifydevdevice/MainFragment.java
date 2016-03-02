ed3e63509f42099553f6849234ca430760bd87be
Danial Goodwin <danialgoodwin@gmail.com>
2016-03-01 20:17:50 -0500

    Add scan all apps button

--- anti-idd-app/src/main/java/com/danialgoodwin/antiidentifydevdevice/MainFragment.java
+++ anti-idd-app/src/main/java/com/danialgoodwin/antiidentifydevdevice/MainFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -25,9 +26,10 @@ public class MainFragment extends Fragment {
 
     private Context mContext;
     private PackageUtils mPackageUtils;
-    private ScanAppsTask mLoadAppListTask;
+    private @Nullable ScanAppsTask mLoadAppListTask;
     private List<AppModel> mInfectedApps = new ArrayList<>();
 
+    private Button mScanAppsButton;
     private ProgressBar mProgressBar;
     private TextView mProgressTextView;
     private RecyclerView mRecyclerView;
@@ -44,34 +46,13 @@ public class MainFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         mPackageUtils = PackageUtils.getInstance(mContext);
-        mLoadAppListTask = new ScanAppsTask(mPackageUtils, new ScanAppsTask.OnProgressListener() {
-            @Override
-            public void onProgress(int progress, int max, @Nullable AppModel app) {
-                if (progress == 0) {
-                    mProgressBar.setMax(max);
-                    mInfectedApps.clear();
-                    mAdapter.notifyDataSetChanged();
-                    mProgressTextView.setKeepScreenOn(true);
-                } else if (progress < max) {
-                    mProgressTextView.setText("Scanning: " + progress + "/" + max);
-                } else {
-                    mProgressBar.setVisibility(View.GONE);
-                    mProgressTextView.setKeepScreenOn(false);
-                    mProgressTextView.setText("Found " + mInfectedApps.size() + ":");
-                }
-                if (app != null) {
-                    mInfectedApps.add(app);
-                    mAdapter.notifyItemInserted(mInfectedApps.size() - 1);
-                }
-                mProgressBar.setProgress(progress);
-            }
-        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mScanAppsButton = (Button) rootView.findViewById(R.id.scan_apps_button);
         mProgressBar = (ProgressBar) rootView.findViewById(R.id.progress_bar);
         mProgressTextView = (TextView) rootView.findViewById(R.id.progress_text);
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.app_list);
@@ -90,17 +71,53 @@ public class MainFragment extends Fragment {
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setAdapter(mAdapter);
 
-        switch (mLoadAppListTask.getStatus()) {
-            case PENDING:
+        mScanAppsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mLoadAppListTask == null) {
+                    mLoadAppListTask = new ScanAppsTask(mPackageUtils, new ScanAppsTask.OnProgressListener() {
+                        @Override
+                        public void onProgress(int progress, int max, @Nullable AppModel app) {
+                            if (progress == 0) {
+                                mProgressBar.setMax(max);
+                                mInfectedApps.clear();
+                                mAdapter.notifyDataSetChanged();
+                                mProgressTextView.setKeepScreenOn(true);
+                            } else if (progress < max) {
+                                mProgressTextView.setText("Scanning: " + progress + "/" + max);
+                            } else {
+                                mScanAppsButton.setVisibility(View.GONE);
+                                mProgressBar.setVisibility(View.GONE);
+                                mProgressTextView.setKeepScreenOn(false);
+                                mProgressTextView.setText("Found " + mInfectedApps.size() + ":");
+                            }
+                            if (app != null) {
+                                mInfectedApps.add(app);
+                                mAdapter.notifyItemInserted(mInfectedApps.size() - 1);
+                            }
+                            mProgressBar.setProgress(progress);
+                        }
+                    });
+                    mLoadAppListTask.execute();
+                }
+            }
+        });
+
+        if (mLoadAppListTask == null) {
+
+        } else if (mLoadAppListTask != null) {
+            switch (mLoadAppListTask.getStatus()) {
+                case PENDING:
 //                mProgressTextView.setText("Loading...");
-                mLoadAppListTask.execute();
-                break;
-            case RUNNING:
-                break;
-            case FINISHED:
-                mProgressBar.setVisibility(View.GONE);
-                mProgressTextView.setText("Found " + mInfectedApps.size() + ":");
-                break;
+//                mLoadAppListTask.execute();
+                    break;
+                case RUNNING:
+                    break;
+                case FINISHED:
+                    mProgressBar.setVisibility(View.GONE);
+                    mProgressTextView.setText("Found " + mInfectedApps.size() + ":");
+                    break;
+            }
         }
     }
 
@@ -111,7 +128,7 @@ public class MainFragment extends Fragment {
 
     @Override
     public void onDestroy() {
-        if (mLoadAppListTask.getStatus() != AsyncTask.Status.FINISHED) {
+        if (mLoadAppListTask != null && mLoadAppListTask.getStatus() != AsyncTask.Status.FINISHED) {
             mLoadAppListTask.cancel(true);
         }
         super.onDestroy();
